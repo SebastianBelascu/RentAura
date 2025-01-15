3094d2c0ca54e240d2be550acaf30b4fd31b9a3c
belascusebi006 <belascusebi006@gmail.com>
2025-01-16 01:41:16 +0200

    D

--- components/navbar/Navsearch.tsx
+++ components/navbar/Navsearch.tsx
@@ -1,9 +1,10 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Input } from '@/components/ui/input';
 import { usePathname, useSearchParams } from 'next/navigation';
 import { useRouter } from 'next/router';
+import { useDebouncedCallback } from 'use-debounce';
 
 function Navsearch() {
   const searchParams = useSearchParams();
@@ -13,12 +14,32 @@ function Navsearch() {
   const [search, setSearch] = useState(
     searchParams.get('search')?.toString() || ''
   );
+  const handleSearch = useDebouncedCallback((value: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (value) {
+      params.set('search', value);
+    } else {
+      params.delete('search');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 500);
+
+  useEffect(() => {
+    if (!searchParams.get('search')) {
+      setSearch('');
+    }
+  }, [searchParams.get('search')]);
 
   return (
     <Input
       type='text'
       placeholder='find a property...'
       className='max-w-xs dark:bg-muted'
+      onChange={(e) => {
+        setSearch(e.target.value);
+        handleSearch(e.target.value);
+      }}
+      value={search}
     />
   );
 }
