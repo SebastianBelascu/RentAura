e9dce80cfc3e1e59895f3d5625d760fa527d71e1
belascusebi006 <belascusebi006@gmail.com>
2025-01-17 01:51:24 +0200

    D

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,3 +1,5 @@
+import FavoriteToggleButton from '@/components/card/FavoriteToggleButton';
+import BreadCrumbs from '@/components/properties/BreadCrumbs';
 import { fetchPropertyDetails } from '@/utils/actions';
 import { redirect } from 'next/navigation';
 import React from 'react';
@@ -7,7 +9,17 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
   if (!property) redirect('/');
   const { baths, bedrooms, beds, guests } = property;
   const details = { baths, bedrooms, beds, guests };
-  return <div></div>;
+  return (
+    <section>
+      <BreadCrumbs name={property.name} />
+      <header className='flex justify-between items-center mt-4'>
+        <h1 className='text-4xl font-bold'>{property.tagline}</h1>
+        <div className='flex items-center gap-x-4'>
+          <FavoriteToggleButton propertyId={property.id} />
+        </div>
+      </header>
+    </section>
+  );
 }
 
 export default PropertyDetailsPage;
