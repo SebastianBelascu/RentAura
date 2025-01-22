aaec0ad3799a35bdb7d15f09e65301b41fb2656d
belascusebi006 <belascusebi006@gmail.com>
2025-01-22 21:54:04 +0200

    D

--- components/card/CountryFlagAndName.tsx
+++ components/card/CountryFlagAndName.tsx
@@ -5,12 +5,12 @@ function CountryFlagAndName({ countryCode }: { countryCode: string }) {
   const validCountry = findCountryByCode(countryCode);
 
   const countryName =
-    validCountry?.name.length > 20
+    validCountry && validCountry?.name.length > 20
       ? `${validCountry?.name.substring(0, 20)}...`
       : validCountry?.name;
   return (
     <span className='flex justify-between items-center gap-2 text-sm'>
-      {validCountry.flag} {countryName}
+      {validCountry && validCountry.flag} {countryName}
     </span>
   );
 }
