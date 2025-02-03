458e4d0e937326346b24e72c1270aa177eaf6300
belascusebi006 <belascusebi006@gmail.com>
2025-02-03 23:49:27 +0200

    D

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import ChartsContainer from '@/components/admin/ChartsContainer';
+import {
+  ChartsLoadingContainer,
+  StatsLoadingContainer,
+} from '@/components/admin/Loading';
+import StatsContainer from '@/components/admin/StatsContainer';
+import { Suspense } from 'react';
 
 function AdminPage() {
-  return <div></div>;
+  return (
+    <>
+      <Suspense fallback={<StatsLoadingContainer />}>
+        <StatsContainer />
+      </Suspense>
+      <Suspense fallback={<ChartsLoadingContainer />}>
+        <ChartsContainer />
+      </Suspense>
+    </>
+  );
 }
 
 export default AdminPage;
