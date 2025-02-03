d676324fecbab45e297e10ade61abc343073eb2c
belascusebi006 <belascusebi006@gmail.com>
2025-02-03 23:44:36 +0200

    D

---
+++ app/reservations/loading.tsx
@@ -0,0 +1,8 @@
+import LoadingTable from '@/components/booking/LoadingTable';
+import React from 'react';
+
+function loading() {
+  <LoadingTable />;
+}
+
+export default loading;
---
+++ components/admin/ChartsContainer.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ChartsContainer() {
+  return <div></div>;
+}
+
+export default ChartsContainer;
